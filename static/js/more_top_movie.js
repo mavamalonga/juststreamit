6f26bf58d393af7b70a7c2e2f8ad2db88108bead
Alpha <mavamalonga.alpha@gmail.com>
2021-10-20 15:35:04 +0200

    add function create_top_movie_section

--- static/js/more_top_movie.js
+++ static/js/more_top_movie.js
@@ -1,10 +1,19 @@
 function more_top_movie(){
-  var movie_id = document.getElementsByClassName("more_top_movie")[0].getAttribute("id");
+  // recovery of container elements
+  var div_top_rated = document.getElementsByClassName("top_rated")[0];
+  var div_top_movie = document.getElementsByClassName("top_movie")[0];
+
+  // build requete
+  var move_id = document.getElementsByClassName("btn_more")[0].getAttribute("id");
   var endpoint = "http://localhost:8000/api/v1/titles/"
   var url = endpoint.concat('', movie_id);
+
+   // delete
+  div_top_rated = div_top_rated.removeChild(div_top_movie);
+
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
-         });
+      });
 };
--- static/js/top_movie.js
+++ static/js/top_movie.js
@@ -1,6 +1,6 @@
 var url = "http://localhost:8000/api/v1/titles/?imdb_score_min=9.5&imdb_score_max=10"
 
-function get_movie_imdb_score_max(url){
+function select_movie_imdb_max(url){
   return fetch(url).then(function(response) {
     return response.json();
   }).then(function(json) {
@@ -8,7 +8,7 @@ function get_movie_imdb_score_max(url){
   });
 }
 
-function select_movie_with_max_votes(movies){
+function choice_best_movie(movies){
   var max_votes = 0;
   var movie_id = 0;
   for (let i = 0; i < movies.length; i++) {
@@ -22,14 +22,67 @@ function select_movie_with_max_votes(movies){
   return movie_id;
 };
 
-function get_movie(movie_id){
+function create_top_movie_section(){
+  // get node 
+  var div_top_rated = document.getElementsByClassName("top_rated")[0];
+  console.log(div_top_rated)
+
+  // div top movie
+  var div_top_movie = document.createElement('div');
+  var attr_class = document.createAttribute('class');
+  attr_class.value = "top_movie";
+  div_top_movie.setAttributeNode(attr_class);
+
+  // poster
+  var img = document.createElement('img');
+  var attr_class = document.createAttribute("class");
+  attr_class.value = "poster";
+  img.setAttributeNode(attr_class);
+  div_top_movie.appendChild(img);
+
+  // title
+  var title = document.createElement('h1');
+  var attr_class = document.createAttribute('class');
+  attr_class.value = "title";
+  title.setAttributeNode(attr_class);
+  div_top_movie.appendChild(title);
+
+  // description
+  var description = document.createElement("p");
+  var attr_class = document.createAttribute("class");
+  attr_class.value = "description";
+  description.setAttributeNode(attr_class);
+  div_top_movie.appendChild(description);
+
+  // button : more
+  var button = document.createElement("input");
+  var attr_type = document.createAttribute("type");
+  attr_type.value = "button"
+  var attr_class = document.createAttribute("class");
+  attr_class.value = "btn_more";
+  var attr_onclick = document.createAttribute("onclick");
+  attr_onclick.value = "more_top_movie()";
+  var attr_value = document.createAttribute("value");
+  attr_value.value = "More";
+  button.setAttributeNode(attr_type);
+  button.setAttributeNode(attr_class);
+  button.setAttributeNode(attr_onclick);
+  button.setAttributeNode(attr_value);
+  div_top_movie.appendChild(button);
+
+  // insert elemnets
+  div_top_rated.insertBefore(div_top_movie, div_top_rated.children[1]);
+}
+
+function get_data_best_movie(movie_id){
   var endpoint = "http://localhost:8000/api/v1/titles/"
   var url = endpoint.concat('', movie_id);
   var div_top_movie = document.getElementsByClassName('top_movie')[0];
+  console.log(div_top_movie);
   var poster = div_top_movie.getElementsByClassName("poster")[0];
   var title = div_top_movie.getElementsByClassName("title")[0];
   var description = div_top_movie.getElementsByClassName("description")[0];
-  var button = div_top_movie.getElementsByClassName("more_top_movie")[0];
+  var button = div_top_movie.getElementsByClassName("btn_more")[0];
   fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -44,8 +97,11 @@ function get_movie(movie_id){
     });
 };
 
-get_movie_imdb_score_max(url).then(function(result) {
-  movie_id = select_movie_with_max_votes(result['results']);
-  get_movie(movie_id);
 
-});
+
+select_movie_imdb_max(url).then(function(result) {
+    movie_id = choice_best_movie(result['results']);
+    create_top_movie_section();
+    get_data_best_movie(movie_id);
+
+  });
